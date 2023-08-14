89a31ff2a2c6c8db5d234c051551740cc992c046
dhillenbp179 <dhillen.mutu@gmail.com>
2023-08-14 11:58:49 +0700

    fix hover on navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,14 @@ export default function Header() {
   };
 
   const isActive = (path) => {
-    // console.log("path: ", location.pathname);
     return location.pathname === path;
   };
+
+  const navLinks = [
+    { path: "/", text: "Home" },
+    { path: "/education", text: "Education" },
+    { path: "/project", text: "Project" },
+  ];
 
   return (
     <header className="group fixed top-0 left-0 right-0 z-10 flex items-center justify-between px-7 py-5 bg-gradient-to-r from-green-600 via-green-700 to-green-800 text-white shadow-lg md:px-10 lg:px-32">
@@ -25,49 +30,29 @@ export default function Header() {
       </div>
       <nav className="hidden md:flex">
         <ul className="flex space-x-8 font-semibold">
-          <li>
-            <a
-              onClick={() => {
-                navigate(`/`);
-              }}
-              className={`cursor-pointer pb-1  ${
-                isActive("/") ? "border-b" : "border-b-0"
-              } hover:font-bold hover:border-b`}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                navigate(`/education`);
-              }}
-              className={`cursor-pointer pb-1  ${
-                isActive("/education") ? "border-b" : "border-b-0"
-              } hover:font-bold hover:border-b`}
-            >
-              Education
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                navigate(`/project`);
-              }}
-              className={`cursor-pointer pb-1  ${
-                isActive("/project") ? "border-b" : "border-b-0"
-              } hover:font-bold hover:border-b`}
-            >
-              Project
-            </a>
-          </li>
+          {navLinks.map((link) => (
+            <li key={link.path}>
+              <a
+                onClick={() => {
+                  navigate(link.path);
+                }}
+                className={`cursor-pointer pb-1 ${
+                  isActive(link.path) ? "border-b" : "border-b-0"
+                } hover:font-bold hover:border-b`}
+              >
+                {link.text}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
       {/* <!-- Hamburger menu for small screens --> */}
       <div className="md:hidden relative">
         <button
           id="hamburgerButton"
-          className="text-white focus:outline-none transition-transform"
+          className={`text-white focus:outline-none transition-transform ${
+            menuOpen ? "shadow-xl p-1 bg-green-900" : ""
+          }`}
           onClick={toggleMenu}
         >
           <div className="w-6 h-[3px] bg-white"></div>
@@ -80,30 +65,17 @@ export default function Header() {
             menuOpen ? "" : "hidden"
           } absolute bg-green-100 mt-2 w-56 py-2 rounded-md shadow-lg right-0 top-4`}
         >
-          <a
-            href="#"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 border-b border-green-600 pb-1 hover:font-bold hover:border-b"
-          >
-            Home
-          </a>
-          <a
-            href="./education.html"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Education
-          </a>
-          <a
-            href="./project.html"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Project
-          </a>
-          <a
-            href="#"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-          >
-            Resume
-          </a>
+          {navLinks.map((link) => (
+            <a
+              key={link.path}
+              href={link.path}
+              className={`block px-4 py-2 text-slate-800 hover:bg-slate-100 border-b-green-500 cursor-pointer pb-1 w-auto transition duration-500 ${
+                isActive(link.path) ? "inline-block border-b " : "border-b-0"
+              } hover:font-bold hover:block hover:border-b`}
+            >
+              {link.text}
+            </a>
+          ))}
         </div>
       </div>
     </header>
